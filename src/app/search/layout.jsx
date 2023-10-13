89dca85797886fd61fab0a3d4573bd207ad7e230
randomgod <tabishnizami456@gmail.com>
2023-10-13 20:23:29 +0530

    add

---
+++ src/app/search/layout.jsx
@@ -0,0 +1,20 @@
+import SearchHeader from "@/components/SearchHeader";
+import "./../globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "WebPulse",
+  description: "Search Engine App",
+};
+
+export default function SearchLayout({ children }) {
+  return (
+    <div className={inter.className}>
+      <SearchHeader />
+
+      {children}
+    </div>
+  );
+}
